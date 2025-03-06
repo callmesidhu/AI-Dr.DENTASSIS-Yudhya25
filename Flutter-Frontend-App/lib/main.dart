a318a93cf358a136eec03fcfd5c9115929e058fa
S SIDHARTH <sidharthpunalur@gmail.com>
2025-03-06 07:59:26 +0530

    Flutter app added

---
+++ Flutter-Frontend-App/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(title: Text("My Flutter App")),
+        body: Center(child: Text("Dr.DENTASSIS APP")),
+      ),
+    );
+  }
+}
